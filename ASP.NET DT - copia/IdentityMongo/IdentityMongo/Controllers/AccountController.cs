5d5731177ebbfc895f310baa9aeb1090ea550e8a
Dillanjtc <157619515+Dillanjtc@users.noreply.github.com>
2024-08-13 21:14:29 -0600

    v1

---
+++ ASP.NET DT - copia/IdentityMongo/IdentityMongo/Controllers/AccountController.cs
@@ -0,0 +1,110 @@
+﻿using IdentityMongo.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Facebook;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Security.Claims;
+
+namespace IdentityMongo.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly SignInManager<ApplicationUser> signInManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login([Required][EmailAddress] string email, [Required] string password, string? returnurl)
+        {
+            if (ModelState.IsValid)
+            {
+                ApplicationUser appUser = await userManager.FindByEmailAsync(email);
+                if (appUser != null)
+                {
+                    await signInManager.SignOutAsync();
+                    var result = await signInManager.PasswordSignInAsync(appUser, password, false, false);
+                    if (result.Succeeded)
+                    {
+                        TempData["WelcomeMessage"] = $"Welcome: {appUser.UserName}";
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+                ModelState.AddModelError(nameof(email), "Login Failed: Invalid Email or Password");
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult LoginWithFacebook()
+        {
+            var redirectUrl = Url.Action("FacebookResponse", "Account");
+            var properties = signInManager.ConfigureExternalAuthenticationProperties(FacebookDefaults.AuthenticationScheme, redirectUrl);
+            return Challenge(properties, FacebookDefaults.AuthenticationScheme);
+        }
+        [Authorize]
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Index", "Home");
+        }
+
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> FacebookResponse()
+        {
+            var result = await HttpContext.AuthenticateAsync(IdentityConstants.ExternalScheme);
+            if (result?.Succeeded != true)
+                return RedirectToAction(nameof(Login));
+
+            var info = await signInManager.GetExternalLoginInfoAsync();
+            if (info == null)
+                return RedirectToAction(nameof(Login));
+
+            var signInResult = await signInManager.ExternalLoginSignInAsync(info.LoginProvider, info.ProviderKey, isPersistent: false, bypassTwoFactor: true);
+            if (signInResult.Succeeded)
+            {
+                TempData["WelcomeMessage"] = $"Welcome: {info.Principal.Identity.Name}";
+                return RedirectToAction("Index", "Home");
+            }
+
+
+            var email = result.Principal.FindFirstValue(System.Security.Claims.ClaimTypes.Email);
+            var user = new ApplicationUser { UserName = email, Email = email };
+            var identityResult = await userManager.CreateAsync(user);
+            if (identityResult.Succeeded)
+            {
+                identityResult = await userManager.AddLoginAsync(user, info);
+                if (identityResult.Succeeded)
+                {
+                    await signInManager.SignInAsync(user, isPersistent: false);
+                    TempData["WelcomeMessage"] = $"Welcome: {user.UserName}";
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+
+            return RedirectToAction(nameof(Login));
+        }
+
+        
+    }
+}
---
+++ ASP.NET DT - copia/IdentityMongo/IdentityMongo/Program.cs
@@ -0,0 +1,61 @@
+using IdentityMongo.Models;
+using IdentityMongo.Settings;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Facebook;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var mongoDbSettings = builder.Configuration.GetSection(nameof(MongoDbConfig)).Get<MongoDbConfig>();
+
+builder.Services.AddIdentity<ApplicationUser, ApplicationRole>()
+       .AddMongoDbStores<ApplicationUser, ApplicationRole, Guid>
+       (
+           mongoDbSettings.ConnectionString, mongoDbSettings.Name
+       )
+       .AddDefaultTokenProviders();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = FacebookDefaults.AuthenticationScheme;
+})
+.AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
+{
+    options.ExpireTimeSpan = TimeSpan.FromMinutes(1);
+    options.SlidingExpiration = true;
+})
+.AddFacebook(options =>
+{
+    options.AppId = "496842542749214";
+    options.AppSecret = "87f393a9450d58a2c69436454f4cb03a";
+});
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
